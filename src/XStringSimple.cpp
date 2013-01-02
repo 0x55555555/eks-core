6b31100913bac09088feceab8ee1ae3ae6332add
Jorj Pimm <jorjpimm@gmail.com>
2013-01-02 19:27:10 +0000

    added missing files

---
+++ src/XStringSimple.cpp
@@ -0,0 +1,24 @@
+#include "XStringSimple"
+#include "QtCore/QHash"
+
+QDataStream &operator>>(QDataStream &str, Eks::Char &vec)
+  {
+  xCompileTimeAssert(sizeof(Eks::Char) == sizeof(xuint8));
+
+  xuint8 out;
+  str >> out;
+  vec = out;
+  return str;
+  }
+
+QDataStream &operator<<(QDataStream &str, Eks::Char vec)
+  {
+  xCompileTimeAssert(sizeof(Eks::Char) == sizeof(xuint8));
+  str << (xuint8)vec;
+  return str;
+  }
+
+uint qHash(const Eks::String &a)
+  {
+  return qHash(a.data());
+  }
