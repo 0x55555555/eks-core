0c18e2fd6022a475dcb467d0bd6b844346ee3d18
George Pimm <jorjpimm@gmail.com>
2014-12-23 10:20:32 +0000

    missed file last comit

---
+++ include/Containers/XStringBuilder.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "Memory/XTemporaryAllocator.h"
+#include "Containers/XStringSimple.h"
+#include "Containers/XStringBuffer.h"
+
+namespace Eks
+{
+
+class StringBuilder
+  {
+public:
+  StringBuilder()
+      : _alloc(Eks::Core::temporaryAllocator()),
+        _buf(&_alloc)
+    {
+    _buf.reserve(1024);
+    }
+
+  StringBuilder(StringBuilder &&oth)
+    {
+    _alloc = std::move(oth._alloc);
+    _buf = std::move(oth._buf);
+    }
+
+  StringBuilder &operator=(StringBuilder &&oth)
+    {
+    _alloc = std::move(oth._alloc);
+    _buf = std::move(oth._buf);
+
+    return *this;
+    }
+
+  void clear()
+    {
+    _buf.clear();
+    }
+
+  template <typename It>
+  void resizeAndCopy(String::size_type newSize, It val)
+    {
+    _buf.resizeAndCopy(newSize, val);
+    }
+
+  template <typename T> StringBuilder &operator<<(const T &t)
+    {
+    _buf.appendType(t);
+
+    return *this;
+    }
+
+  operator Eks::String() const
+    {
+    return Eks::String(_buf, Core::defaultAllocator());
+    }
+
+  Eks::String value(Eks::AllocatorBase *a) const
+    {
+    return Eks::String(_buf, a);
+    }
+
+  const Eks::String &value() const
+    {
+    return _buf;
+    }
+
+  const char *data() const
+    {
+    return _buf.data();
+    }
+
+  xsize length() const
+    {
+    return _buf.length();
+    }
+
+  template <typename T> void adjust(const T &t)
+    {
+    t(_buf);
+    }
+
+private:
+  Eks::TemporaryAllocator _alloc;
+  Eks::String _buf;
+  };
+
+}
